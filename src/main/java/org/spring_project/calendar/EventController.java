55a9a2b968dd1866a576747759284e0b4cc97e44
Bernard Joyce <joycebernie833@gmail.com>
2026-05-09 17:20:51 +0100

    Posting an event

--- src/main/java/org/spring_project/calendar/EventController.java
+++ src/main/java/org/spring_project/calendar/EventController.java
@@ -1,11 +1,15 @@
 package org.spring_project.calendar;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EventController {
@@ -16,7 +20,7 @@ public class EventController {
     }
 
     @PostMapping("/event")
-    public ResponseEntity<String> makeEvent(RequestEntity<Event> request){
-
+    public ResponseEntity<String> makeEvent(@RequestBody String event, @RequestBody LocalDate date) {
+        return new ResponseEntity<>("The event has been added!", HttpStatus.OK);
     }
 }
