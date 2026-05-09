d93dd4cfd9f86d5a99b5c700593c209f8e02c091
Bernard Joyce <joycebernie833@gmail.com>
2026-05-09 17:15:46 +0100

    Initial commit

---
+++ src/main/java/org/spring_project/calendar/Event.java
@@ -0,0 +1,6 @@
+package org.spring_project.calendar;
+
+import java.time.LocalDate;
+
+public record Event(String event, LocalDate date) {
+}
---
+++ src/test/java/org/spring_project/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring_project.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
