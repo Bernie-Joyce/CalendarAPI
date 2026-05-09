1f3046e3228ec00c880435833abbc9dfad85fe54
Bernard Joyce <joycebernie833@gmail.com>
2026-05-09 21:00:39 +0100

    Added tags for OpenAPI

--- src/main/java/org/spring_project/calendar/EventController.java
+++ src/main/java/org/spring_project/calendar/EventController.java
@@ -1,5 +1,6 @@
 package org.spring_project.calendar;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +17,7 @@ public class EventController {
         this.repository = repository;
     }
 
-    @GetMapping("/event/today")
-    public ResponseEntity<List<Event>> getTodayEvents() {
-        List<Event> events = repository.findAllByDate(LocalDate.now());
-        if (events.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(events, HttpStatus.OK);
-    }
-
+    @Tag(name = "Get all events")
     @GetMapping("/event")
     public ResponseEntity<List<Event>> getEvents() {
         List<Event> events = repository.findAll();
@@ -34,18 +27,43 @@ public class EventController {
         return new ResponseEntity<>(events, HttpStatus.OK);
     }
 
-    @PostMapping("/event")
-    public ResponseEntity<Event> makeEvent(@RequestBody Event event) {
-        Event saved = repository.save(event);
-        return new ResponseEntity<>(saved, HttpStatus.OK);
+    @Tag(name = "Get today's events")
+    @GetMapping("/event/today")
+    public ResponseEntity<List<Event>> getTodayEvents() {
+        List<Event> events = repository.findAllByDate(LocalDate.now());
+        if (events.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(events, HttpStatus.OK);
     }
 
+    @Tag(name="Get an event by Id")
     @GetMapping("/event/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable Integer id) {
         Optional<Event> event = repository.findById(id);
         return event.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Tag(name = "Get events between two dates")
+    @GetMapping("/event/{start}/{end}")
+    public ResponseEntity<List<Event>> getEventsBetweenDates(@PathVariable LocalDate start, @PathVariable LocalDate end) {
+        List<Event> events = repository.findAllByDateBetween(start, end);
+        if (events.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(events, HttpStatus.OK);
+    }
+
+
+    @Tag(name = "Create a new event")
+    @PostMapping("/event")
+    public ResponseEntity<Event> makeEvent(@RequestBody Event event) {
+        Event saved = repository.save(event);
+        return new ResponseEntity<>(saved, HttpStatus.OK);
+    }
+
+
+    @Tag(name = "Delete an event")
     @DeleteMapping("/event/{id}")
     public ResponseEntity<Event> deleteEventById(@PathVariable Integer id) {
         Optional<Event> event = repository.findById(id);
@@ -56,12 +74,14 @@ public class EventController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/event/{start}/{end}")
-    public ResponseEntity<List<Event>> getEventsBetweenDates(@PathVariable LocalDate start, @PathVariable LocalDate end) {
-        List<Event> events = repository.findAllByDateBetween(start, end);
-        if (events.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(events, HttpStatus.OK);
+
+    @Tag(name = "Update an event")
+    @PutMapping("/event/{id}")
+    public ResponseEntity<Event> updateEvents(@PathVariable Integer id, @RequestBody Event event) {
+        return repository.findById(id).map(e -> {
+            e.setEvent(event.getEvent());
+            e.setDate(event.getDate());
+            return new ResponseEntity<>(repository.save(e), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/org/spring_project/calendar/EventRepository.java
+++ src/main/java/org/spring_project/calendar/EventRepository.java
@@ -1,6 +1,8 @@
 package org.spring_project.calendar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Integer> {
     List<Event> findAllByDate(LocalDate date);
     List<Event> findAllByDateBetween(LocalDate start, LocalDate end);
+
 }
