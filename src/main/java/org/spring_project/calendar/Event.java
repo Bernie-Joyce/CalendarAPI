922eba8d39d6f3b2cba45dd343265c25074f605b
Bernard Joyce <joycebernie833@gmail.com>
2026-05-13 21:54:05 +0100

    simplifying jpa entities with lombok

--- src/main/java/org/spring_project/calendar/Event.java
+++ src/main/java/org/spring_project/calendar/Event.java
@@ -5,75 +5,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.jspecify.annotations.NonNull;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Integer id;
-    private  @NonNull String event;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private  @NonNull LocalDate date;
+    private Integer id;
 
-    public Event(@NonNull  String event, @JsonFormat(pattern = "yyyy-MM-dd") @NonNull LocalDate date) {
+    @NotBlank(message = "Event name cannot be blank")
+    private String event;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Date cannot be null")
+    private LocalDate date;
+
+    public Event(@NonNull String event, @NonNull LocalDate date) {
         this.event = event;
         this.date = date;
     }
-
-    public Event() {
-
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public @NonNull String getEvent() {
-        return event;
-    }
-
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    public @NonNull LocalDate getDate() {
-        return date;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setEvent(@NonNull String event) {
-        this.event = event;
-    }
-
-    public void setDate(@NonNull LocalDate date) {
-        this.date = date;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Event) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.event, that.event) &&
-                Objects.equals(this.date, that.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, event, date);
-    }
-
-    @Override
-    public String toString() {
-        return "Event[" +
-                "id=" + id + ", " +
-                "event=" + event + ", " +
-                "date=" + date + ']';
-    }
-
 }
