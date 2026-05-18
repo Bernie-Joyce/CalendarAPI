f94b00df1bc229c25f6eb676f9aa1e32dd14ee26
Bernard Joyce <joycebernie833@gmail.com>
2026-05-18 10:00:14 +0100

    Refactoring way tokens work for simpler tokens that work

--- src/main/java/org/spring_project/calendar/User/UserDTO.java
+++
@@ -1,4 +0,0 @@
-package org.spring_project.calendar.User;
-
-public record UserDTO(String username, String password) {
-}
--- src/main/java/org/spring_project/calendar/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.spring_project.calendar.User;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<App_User, Integer> {
-    Optional<App_User> findByUsername(String username);
-}
